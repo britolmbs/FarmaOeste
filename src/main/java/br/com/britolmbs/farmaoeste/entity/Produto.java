e412f069dffdfc4943cdb4f87bfac9ae2f1f2b2a
Lucas Brito <britolmbs@hotmail.com>
2025-11-03 15:31:34 -0300

    Feat: Criação do Entity Produto com Getter e Setter

---
+++ src/main/java/br/com/britolmbs/farmaoeste/entity/Produto.java
@@ -0,0 +1,71 @@
+package br.com.britolmbs.farmaoeste.entity;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "produto")
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private String nome;
+    private String descricao;
+    private double preco;
+    private int quantidade_estoque;
+
+    @CreatedDate
+    private LocalDate data_criacao;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade_estoque() {
+        return quantidade_estoque;
+    }
+
+    public void setQuantidade_estoque(int quantidade_estoque) {
+        this.quantidade_estoque = quantidade_estoque;
+    }
+
+    public LocalDate getData_criacao() {
+        return data_criacao;
+    }
+
+    public void setData_criacao(LocalDate data_criacao) {
+        this.data_criacao = data_criacao;
+    }
+}
