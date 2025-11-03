d85cea3e9c8ddd7dc7fd17420e0ea986d8d2be15
Lucas Brito <britolmbs@hotmail.com>
2025-11-03 10:26:14 -0300

    Inicio do projeto

---
+++ src/main/java/br/com/britolmbs/farmaoeste/FarmaoesteApplication.java
@@ -0,0 +1,13 @@
+package br.com.britolmbs.farmaoeste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaoesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaoesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/britolmbs/farmaoeste/FarmaoesteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.britolmbs.farmaoeste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaoesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
