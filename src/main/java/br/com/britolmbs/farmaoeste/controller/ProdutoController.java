02a7baa40060a1067250f8bfbe282a08b1168c8b
Lucas Brito <britolmbs@hotmail.com>
2025-11-04 15:14:15 -0300

    Feat: Inicio da Criação do Controller do produto

---
+++ src/main/java/br/com/britolmbs/farmaoeste/controller/ProdutoController.java
@@ -0,0 +1,22 @@
+package br.com.britolmbs.farmaoeste.controller;
+
+import br.com.britolmbs.farmaoeste.entity.Produto;
+import br.com.britolmbs.farmaoeste.useCases.CadatroProdutoUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/produto")
+@RestController
+public class ProdutoController {
+
+    @Autowired
+    CadatroProdutoUseCase cadatroProdutoUseCase;
+
+    @PostMapping("/create")
+    public void create(@RequestBody Produto produto){
+        cadatroProdutoUseCase.execute(produto);
+    }
+}
---
+++ src/main/java/br/com/britolmbs/farmaoeste/useCases/CadatroProdutoUseCase.java
@@ -0,0 +1,11 @@
+package br.com.britolmbs.farmaoeste.useCases;
+
+import br.com.britolmbs.farmaoeste.entity.Produto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CadatroProdutoUseCase {
+    public void execute(Produto produto) {
+
+    }
+}
